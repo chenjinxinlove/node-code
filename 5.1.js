384ba5cbaeee55318dc6b943f79384f288709c38
macchen <chenjinxinlove@gamil.com>
2017-06-24 15:07:19 +0800

    5.5.js

---
+++ 5.1.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+
+console.info("----- process cwd ------");
+console.info();
+console.log('Current dirextory:' + process.cwd());
+console.log('change directory to: ', path.join(__dirname , '/chen') )
+
+console.log('current directory'+ process.cwd())
+console.info();
+console.info("----- process cwd ------");
---
+++ 5.3.js
@@ -0,0 +1,11 @@
+// console.log(process.pid);
+// console.log(process.title);
+// console.log(process.version);
+// console.log(process.versions);
+// console.log(process.config);
+// console.log(process.execPath);
+// console.log(process.argv);
+// console.log(process.platform);
+// console.log(process.arch);
+// console.log(process.env);
+process.stdout.write('ddddddd');
